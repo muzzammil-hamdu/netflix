38d66d5c72c0e92f002c7235a22c3db5538d7996
muzzammil-hamdu <shaikhussain752@gmail.com>
2026-03-11 23:16:53 +0530

    Refactor next.config.js to simplify export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  reactStrictMode: true,
   images: {
     unoptimized: true
   }
 }
-
-module.exports = nextConfig

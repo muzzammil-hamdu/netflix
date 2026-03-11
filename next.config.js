5f3c2fed42493d262bc58f38fdea5f9d7c8e6f20
muzzammil-hamdu <shaikhussain752@gmail.com>
2026-03-11 23:16:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true
+  }
 }
+
+module.exports = nextConfig
